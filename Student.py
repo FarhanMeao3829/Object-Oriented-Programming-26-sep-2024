66816c1c98d3a31c8fe7e8ec02f922b3d9877cb0
Farhan Shafiq Chowdhury <yfarhana044@gmail.com>
2024-09-26 10:07:40 +0600

    first commit

---
+++ Student.py
@@ -0,0 +1,22 @@
+class Fruit:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+        
+apple = Fruit("Appy", "Red")
+print(apple.name)
+print(apple.color)
+
+orange = Fruit("Orangyyyyyyyyyyyyyyyyyyyyyy", "Orange")
+print(orange.name)
+print(orange.color)
+
+#=========================================================
+
+class Student:
+    grade = 10
+    print("Sup I'm a student of grade 10")
+    
+    
+ob = Student()
+        
